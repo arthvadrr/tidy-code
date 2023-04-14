555c9aac42cc28b7632c8c37952b2ccc8d2ceb32
Bryan Patrick <bpatrick@ou.edu>
2023-04-13 23:56:41 -0500

    Working new command

---
+++ src/defaultPreferences.ts
@@ -0,0 +1,5 @@
+export const defaultPreferences: object = {
+    formatOnSave: true,
+};
+
+export default defaultPreferences;
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,21 @@
 import * as vscode from 'vscode';
+import defaultPreferences from './defaultPreferences';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "tidy-code" is now active!');
 
 	let disposable = vscode.commands.registerCommand('tidy-code.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from tidy-code!');
+		vscode.window.showInformationMessage('Hello World jf tidy-code!');
+		
 	});
 
-	context.subscriptions.push(disposable);
+	const formatCode = () => {
+		console.log('hi');
+	};
+
+	const formatCodeCommand = vscode.commands.registerCommand('tidy-code.formatCode', formatCode);
+	
+	context.subscriptions.push(formatCodeCommand);
 }
 
 export function deactivate() {}
