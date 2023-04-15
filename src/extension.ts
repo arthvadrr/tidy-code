34a15dcdb288f6aebc269583b3c11327f1143421
Bryan Patrick <bpatrick@ou.edu>
2023-04-15 11:29:18 -0500

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import defaultPreferences from './defaultPreferences';
 
+// to store preferences maybe? https://code.visualstudio.com/api/references/vscode-api#ExtensionContext.globalState
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "tidy-code" is now active!');
 
@@ -10,11 +12,11 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	const formatCode = () => {
-		console.log('hi');
+		console.log(vscode.);
 	};
 
 	const formatCodeCommand = vscode.commands.registerCommand('tidy-code.formatCode', formatCode);
-	
+
 	context.subscriptions.push(formatCodeCommand);
 }
 
